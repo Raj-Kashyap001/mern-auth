666aecb6807ddfe4044d9a8aba67ba28287cf3d9
Raj Kashyap <rajkashyapatservice@outlook.com>
2025-02-08 19:55:58 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
+import { configDotenv } from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
+configDotenv();
 
+configDotenv;
 const app = express();
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected To MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get("/", (req, res) => {
   res.status(200).json({
